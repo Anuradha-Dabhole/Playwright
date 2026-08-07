7ae846596c6e5af274ba6f099ee1b1fd376a18cc
Anuradha Dabhole <anuradhadabhole5020@gmail.com>
2026-08-07 11:52:37 +0530

    Day07 agenda

--- Day02Assignment/StudentManagementSystem.js
+++ Day02Assignment/StudentManagementSystem.js
@@ -1,25 +1,23 @@
-// class Student
-// {
-//     constructor(name,age)
-//     {
-//         this.name=name;
-//         this.age=age;
-//     }
-//     display()
-//     {
-//         console.log(`Name :${this.name}`);
-//         console.log(`Age : ${this.age}`);
-//     }
-// }
-// let students=[];
-// let s1=new Student("Anu",22);
-// let s2=new Student("Arati",21)
-// let s3=new Student("Raj",20)
-
-// students.push(s1);
-// students.push(s2);
-// students.push(s3);
-// students.forEach(student => {
-//     student.display();
-// });
-
+class Person
+{
+    constructor(name)
+    {
+        this.name=name;
+    }
+}
+class Student extends Person
+{
+    
+    #studentID;
+    #grade;
+    constructor(name,studentID,grade)
+    {
+        super(name);
+        this.#studentID=studentID;
+        this.#grade=grade;
+    }
+    get studentID()
+    {
+        return this.#studentID;
+    }
+}
